9aba1a23d8af83583ec535cce818fca15e3c1456
Agustin Allende <agus.allende996@gmail.com>
2025-04-24 10:43:42 -0300

    Fix rutas y configuración para Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,11 +11,7 @@ app.use(express.json());
 
 app.use('/api', apiRoutes); 
 
-app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
-});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Servidor corriendo en el puerto', process.env.PORT || 3000);
