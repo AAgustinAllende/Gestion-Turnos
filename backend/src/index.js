92a9d1837d6002627db71a9723d5601c240d3cd3
Agustin Allende <agus.allende996@gmail.com>
2025-04-24 10:51:41 -0300

    Fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,26 +1,5 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import routerTurnos from './routes/turnos.routes.js';
+import app from "./app.js"
+import './database/connection.js'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const app = express();
-const PORT = process.env.PORT || 4000;
-
-// Middlewares
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '../dist')));
-
-// Rutas API
-app.use('/api', routerTurnos);
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+app.listen(app.get('port'))
+console.log('Server on port', app.get('port'))
