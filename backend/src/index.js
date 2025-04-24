7aeb4900591a8e453fc2aee9871f3707072330a6
Agustin Allende <agus.allende996@gmail.com>
2025-04-24 10:48:14 -0300

    Fix rutas para Render y puerto

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import apiRoutes from './routes/turnero.routes.js';
+import routerTurnos from './routes/turnos.routes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const app = express();
+const PORT = process.env.PORT || 4000;
 
-app.use(express.json()); 
+// Middlewares
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../dist')));
 
-app.use('/api', apiRoutes); 
+// Rutas API
+app.use('/api', routerTurnos);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
 
 
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Servidor corriendo en el puerto', process.env.PORT || 3000);
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
