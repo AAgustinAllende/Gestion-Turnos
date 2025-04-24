53099bafa9ee3a96d1a71a34e483c5a18b1cacad
Agustin Allende <agus.allende996@gmail.com>
2025-04-24 10:33:02 -0300

    Manejo de errores de rutas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import apiRoutes from './routes/turnero.routes.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 
+app.use(express.json()); 
+
+app.use('/api', apiRoutes); 
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Servidor corriendo en el puerto', process.env.PORT || 3000);
 });
